06cb410600feb665b54c93f24298937e25d8751c
mavle0219 <115463935+mavle0219@users.noreply.github.com>
2023-06-06 23:10:03 +0800

    Add files via upload

--- Accounting/mytotolog.php
+++ Accounting/mytotolog.php
@@ -87,7 +87,7 @@ if (!isset($admin)) {
 
         <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
           <div class="app-brand demo">
-            <a href="dashboard.php" class="app-brand-link">
+            <a href="mytotoa.php" class="app-brand-link">
               <span class="app-brand-logo demo">
                 <svg
                   width="25"
